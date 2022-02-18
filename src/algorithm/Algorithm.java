dccae4d0fe92bae1e7999d19851aab0cbde1f9f5
adels <adel.semchenko@gmail.com>
2022-02-18 22:45:39 +0300

    class algorithm input data

---
+++ src/algorithm/Algorithm.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Algorithm {
+    private int[] g; //generating polynomial
+    private int k; //number of information symbols
+    private int[] e; //error vector
+    private int[] l; //information sequence
+    private int r; //degree of the polynomial
+}
